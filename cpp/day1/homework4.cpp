3ebafc97d144b519020ed78595ec9bf97f598705
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-03 20:37:39 +0400

    Update homework4.cpp

--- cpp/day1/homework4.cpp
+++ cpp/day1/homework4.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main () {
         int chickens;
-        cout << "Enter the number of chickens" << endl;
+        cout << "Enter the number of chickens." << endl;
         cin >> chickens;
         int cows;
-        cout << "Enter the number of cows" << endl;
+        cout << "Enter the number of cows." << endl;
         cin >> cows;
         int pigs;
-        cout << "Enter the number of pigs" << endl;
+        cout << "Enter the number of pigs." << endl;
         cin >> pigs;
         int result = 2 * chickens + 4 * cows + 4 * pigs;
         cout << "The result is : " << result << endl;
