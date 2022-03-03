aed98924ad3b853a7d93e6aff54aaef475f45ed2
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-03 20:42:42 +0400

    Update homework4.cpp

--- cpp/day1/homework4.cpp
+++ cpp/day1/homework4.cpp
@@ -12,6 +12,6 @@ int main () {
         cout << "Enter the number of pigs." << endl;
         cin >> pigs;
         int result = 2 * chickens + 4 * cows + 4 * pigs;
-        cout << "The result is : " << result << endl;
+        cout << "The result is " << result << "." << endl;
 	return 0;
 }
