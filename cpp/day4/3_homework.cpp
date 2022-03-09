e6366d11e1ec262357bede3769298041a02cc62e
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-09 18:52:47 +0400

    Update 3_homework.cpp

--- cpp/day4/3_homework.cpp
+++ cpp/day4/3_homework.cpp
@@ -4,23 +4,20 @@ using namespace std;
 int largest(int arr[], int);
 void print(int arr[], int);
 void input(int arr[], int);
-int main()
-{
+int main() {
     int n;
-    cout << "Input array lenght" <<endl;
+    cout << "Input array lenght" << endl;
     cin >> n;
 	int arr[n];
 	input(arr, n);
-	print(arr,n);
+	print(arr, n);
 	cout << largest(arr, n);
 	return 0;
 }
-
 int largest(int arr[], int n)
 {
-	int i;
 	int max = arr[0];
-	for (i = 1; i < n; i++)
+	for (int i = 1; i < n; i++)
 		if (arr[i] > max)
 			max = arr[i];
 	return max;
@@ -32,6 +29,6 @@ void print(int arr[], int n) {
 }
 void input(int arr[], int n) {
     for ( int i = 0; i < n; i++)
-        arr[i] = rand() % 100;;
+        arr[i] = rand() % 100;
     cout << endl;
 }
