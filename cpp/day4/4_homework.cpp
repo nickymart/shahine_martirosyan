cf36f3cfa4ee1ab65899e9b5d161c02e6ff0ab7a
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-09 19:11:03 +0400

    Update 4_homework.cpp

--- cpp/day4/4_homework.cpp
+++ cpp/day4/4_homework.cpp
@@ -6,7 +6,7 @@ void print(int arr[], int);
 void input(int arr[], int);
 int main() {
     int n;
-    cout << "Input array lenght" <<endl;
+    cout << "Input array lenght:";
     cin >> n;
 	int arr[n];
 	input(arr, n);
@@ -35,6 +35,5 @@ void print(int arr[], int n) {
 }
 void input(int arr[], int n) {
     for ( int i = 0; i < n; i++)
-        arr[i] = rand() % 100;;
-    cout << endl;
+        arr[i] = rand() % 100;
 }
