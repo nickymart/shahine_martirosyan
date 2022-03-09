803805cbfaa7f51fb37fcf0d91b286cbc18a9752
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-09 19:05:56 +0400

    Update day1_6_homework.cpp

--- cpp/day3/day1_6_homework.cpp
+++ cpp/day3/day1_6_homework.cpp
@@ -3,14 +3,16 @@ using namespace std;
 int factorial (int);
 int main () {
         int n;
-        cout << "Enter your number." << endl;
-        cin >> n;
+        do {
+            cout << "Enter your number." << endl;
+            cin >> n;
+        } while(n < 0);
         int res = factorial(n);
         cout << "The factorial of " << n << " is " << res << "." << endl; 
         return 0;
 }
 int factorial (int n) {
-    if(n == 1)
+    if(n == 0)
         return 1;
     return n * factorial(n - 1);
 }
