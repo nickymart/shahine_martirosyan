fed5b0f4ec171e35b6f79d6eebe5d3b49e157988
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:45:20 +0400

    Update 6_homework.cpp

--- cpp/day6/6_homework.cpp
+++ cpp/day6/6_homework.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-using namespace std; 
- int main( ) {
-    const int size = 10;
+using namespace std;
+
+ int main() {
+    const int size = 3;
     int matrix[size][size] = {0};
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         for (int j = i; j < size - i; j++) {
-            if(j == 0){
+            if (j == 0) {
                 matrix[i][j] += 1;
             } else {
                 matrix[i][j] = matrix[i][j - 1] + 1;
@@ -14,10 +15,10 @@ using namespace std;
         for (int j = i + 1; j < size - i ; j ++) {
             matrix[j][size - i - 1] = matrix[j - 1][size - i - 1] + 1;
         }
-        for(int j = size - i - 2; j >= i; j --){
+        for (int j = size - i - 2; j >= i; j --) {
             matrix[size - i - 1][j] = matrix[size - i - 1][j + 1] + 1;      
         }
-        for(int j = size - i - 2; j > i; j --){
+        for (int j = size - i - 2; j > i; j --) {
             matrix[j][i] = matrix[j + 1][i] + 1;  
         }
     }
