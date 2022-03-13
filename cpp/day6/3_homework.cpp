d48b9021ce045fc70421e5c6329d00107c35a5d7
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:39:13 +0400

    Update 3_homework.cpp

--- cpp/day6/3_homework.cpp
+++ cpp/day6/3_homework.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int main () {
     int arr[10] = {0};
-    //int elem = 5;
     int i = 0;
-    for(int k = 1; i < 10; k ++){
-        if((1 +  k * k) % 5 == 0){
+    for (int k = 1; i < 10; k ++) {
+        if ((1 +  k * k) % 5 == 0) {
             arr[i] = 1 +  k * k;
+            cout << arr[i] << " ";
             i++;
         }
-    } 
-    for(int i = 0; i < 10; i ++){
-         cout << arr[i] << "  ";    
     }
+    cout << endl;
     return 0;    
 }
-
