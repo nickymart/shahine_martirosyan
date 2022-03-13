fd64dc8b9545a6be9461ba7952906529970f5295
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 19:33:48 +0400

    Update 6_homework.cpp

--- cpp/day6/6_homework.cpp
+++ cpp/day6/6_homework.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
- int main() {
-    const int size = 3;
+void space(int i) {
+    while (i--) {
+        cout << " ";
+    }
+}
+int count(int n) {
+    int i = 1;
+    while(n){
+        i ++;
+        n /= 10; 
+    }
+    return i;
+}
+ int main( ) {
+    const int size = 20;
     int matrix[size][size] = {0};
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++){
         for (int j = i; j < size - i; j++) {
-            if (j == 0) {
+            if(j == 0){
                 matrix[i][j] += 1;
             } else {
                 matrix[i][j] = matrix[i][j - 1] + 1;
@@ -15,16 +28,17 @@ using namespace std;
         for (int j = i + 1; j < size - i ; j ++) {
             matrix[j][size - i - 1] = matrix[j - 1][size - i - 1] + 1;
         }
-        for (int j = size - i - 2; j >= i; j --) {
+        for(int j = size - i - 2; j >= i; j --){
             matrix[size - i - 1][j] = matrix[size - i - 1][j + 1] + 1;      
         }
-        for (int j = size - i - 2; j > i; j --) {
+        for(int j = size - i - 2; j > i; j --){
             matrix[j][i] = matrix[j + 1][i] + 1;  
         }
     }
     for (int i = 0; i < size; i ++){
         for (int j = 0; j < size; j ++){
-            cout << matrix[i][j] << "  ";    
+            cout << matrix[i][j] ;
+            space(count(size * size) - count(matrix[i][j]) + 1);
         }    
         cout << endl;
     }
