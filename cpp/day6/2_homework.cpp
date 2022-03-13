4dcc9294394fe44f93f07bc735759aa35ec17cd9
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:36:26 +0400

    Update 2_homework.cpp

--- cpp/day6/2_homework.cpp
+++ cpp/day6/2_homework.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main () {
-    const int m = 5, n = 6;
+    const int m = 4, n = 5;
     int matrix[m][n];
     cout << "Input:" << endl; 
-    for(int i = 0; i < m; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
             matrix[i][j] = rand() % 10;
             cout << matrix[i][j] << " ";
         }    
@@ -14,14 +14,14 @@ int main () {
     } 
     int min_index, max_index;
     int elem;
-    for(int i = 0; i < m; i ++){
+    for (int i = 0; i < m; i ++) {
         min_index = i;
         max_index = i;
-        for(int j = 0; j < n; j ++){
-            if(matrix[i][min_index] > matrix[i][j]){
+        for (int j = 0; j < n; j ++) {
+            if (matrix[i][min_index] > matrix[i][j]) {
                 min_index = j;    
             }
-            if(matrix[i][max_index] < matrix[i][j]){
+            if (matrix[i][max_index] < matrix[i][j]) {
                 max_index = j;    
             }
         }    
@@ -30,12 +30,11 @@ int main () {
         matrix[i][max_index] = elem;
     }
     cout << "Output:" << endl;
-    for(int i = 0; i < m; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
             cout << matrix[i][j] << " ";
         }    
         cout <<endl;
     }  
     return 0;    
 }
-
