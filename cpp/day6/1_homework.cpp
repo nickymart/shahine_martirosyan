7c9774543c093ba62df9699a6fd83dbae58b3228
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:32:39 +0400

    Update 1_homework.cpp

--- cpp/day6/1_homework.cpp
+++ cpp/day6/1_homework.cpp
@@ -2,17 +2,17 @@
 #include <string>
 using namespace std;
 
-bool is_two_b(string word);
+bool is_two_b(string);
 int main()
 {
     string line;
-    cout << "Enter the line:" ;
+    cout << "Enter the line:";
     getline(cin, line);
-    int count  = 0;
+    int count = 0;
     string word = "";
     line += " ";
-    for(int i = 0; i < line.size(); i++){
-        if(line[i] == ' '){
+    for (int i = 0; i < line.size(); i++) {
+        if (line[i] == ' ') {
             count += is_two_b(word);
             word = "";
             continue;
@@ -24,15 +24,14 @@ int main()
 }
 bool is_two_b(string word){
     int i = 2;
-    for(int j = 0; j < word.size(); j ++){
-        if(word[j] == 'B'){
+    for (int j = 0; j < word.size(); j ++) {
+        if (word[j] == 'B') {
             i--;    
         }    
     }
-    if(i > 0){
+    if (i > 0) {
         return 0;    
-    }
-    else{
+    } else {
         return 1;
     }
 }
