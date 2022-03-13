6761a56961dd742706405a577d4074f40d20c2a0
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 19:58:25 +0400

    Update 5_homework.cpp

--- cpp/day6/5_homework.cpp
+++ cpp/day6/5_homework.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void space(int i);
+int count(int n);
 int main () {
-    const int  n = 4;
+    const int  n = 10;
     int matrix1[n][n];
     int matrix2[n][n];
     int matrix[n][n];
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n; j ++) {
-            matrix1[i][j] = rand() % 11;
-            cout << matrix1[i][j] << " ";
+            matrix1[i][j] = rand() % 10 + 1;
+            cout << matrix1[i][j] ;
+            space(3 - count(matrix1[i][j]) + 1);
         }    
         cout << endl;
     } 
     cout << "+" << endl;
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n; j ++) {
-            matrix2[i][j] = rand() % 11;
-            cout << matrix2[i][j] <<  " ";
+            matrix2[i][j] = rand() % 10 + 1;
+            cout << matrix2[i][j] ;
+            space(3 - count(matrix2[i][j]) + 1);
         }    
         cout << endl;
     } 
@@ -25,9 +29,23 @@ int main () {
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n; j ++) {
             matrix[i][j] = matrix1[i][j] + matrix2[i][j];
-            cout << matrix[i][j] << " ";
+            cout << matrix[i][j] ;
+            space(3 - count(matrix[i][j]) + 1);
         }    
         cout << endl;
     }
     return 0;    
+}
+void space(int i) {
+    while (i--) {
+        cout << " ";
+    }
+}
+int count(int n) {
+    int i = 1;
+    while(n){
+        i ++;
+        n /= 10; 
+    }
+    return i;
 }
