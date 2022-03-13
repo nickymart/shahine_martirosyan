1b2167c71af6c8957986890286ea1fdee3586b00
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:41:26 +0400

    Update 4_homework.cpp

--- cpp/day6/4_homework.cpp
+++ cpp/day6/4_homework.cpp
@@ -5,34 +5,32 @@ int main () {
     const int m = 5, n = 4;
     int matrix[m][n];
     cout << "Input:" << endl; 
-    for(int i = 0; i < m; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
             cin >> matrix[i][j];
         }    
-    } 
+    }  
     int index = -1;;
     bool b = true;
-    for(int i = 0; i < m; i ++){
-        for(int j = 0; j < n; j ++){
-            if(matrix[i][j] % 2 != 0){
-               // cout << "a" << endl;
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
+            if (matrix[i][j] % 2 != 0) {
                 b = false;
             }
         }    
-        if(b){
+        if (b) {
             index = i;
             break;
         }
         b = true;
     }
-    if(index == -1){
+    if (index == -1) {
         cout << "No";
         return 0;
     }
     cout << "Output:" << endl;
-    for(int i = 0; i < n; i ++){
+    for (int i = 0; i < n; i ++) {
         cout << matrix[index][i]<< " ";
     }  
     return 0;    
 }
-
