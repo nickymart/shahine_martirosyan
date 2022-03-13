769f0671dca8896e92614a62e416d93a882a119f
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-13 13:42:44 +0400

    Update 5_homework.cpp

--- cpp/day6/5_homework.cpp
+++ cpp/day6/5_homework.cpp
@@ -6,24 +6,24 @@ int main () {
     int matrix1[n][n];
     int matrix2[n][n];
     int matrix[n][n];
-    for(int i = 0; i < n; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
             matrix1[i][j] = rand() % 11;
             cout << matrix1[i][j] << " ";
         }    
         cout << endl;
     } 
     cout << "+" << endl;
-    for(int i = 0; i < n; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
             matrix2[i][j] = rand() % 11;
             cout << matrix2[i][j] <<  " ";
         }    
         cout << endl;
     } 
     cout << "=" << endl;
-    for(int i = 0; i < n; i ++){
-        for(int j = 0; j < n; j ++){
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
             matrix[i][j] = matrix1[i][j] + matrix2[i][j];
             cout << matrix[i][j] << " ";
         }    
@@ -31,4 +31,3 @@ int main () {
     }
     return 0;    
 }
-
