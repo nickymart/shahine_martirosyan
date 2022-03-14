3142fcac8c6290acc7bc62cd3eba62db1d27ca96
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-14 21:27:04 +0400

    Update 3_homework.cpp

--- cpp/day7/3_homework.cpp
+++ cpp/day7/3_homework.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 void sum_of_array_elements(int arr[], int n, int *sum);
-int main()
-{
+
+int main() {
     const int n = 10;
     int arr[n];
-    for(int i = 0; i < 10; i ++){
+    for (int i = 0; i < 10; i ++) {
         arr[i] = rand() % 9 + 1;
         cout << arr[i] << " ";
     }
@@ -17,9 +17,9 @@ int main()
     cout << *sum << endl;
     return 0;
 }
-void sum_of_array_elements(int arr[], int n, int *sum){
+void sum_of_array_elements (int arr[], int n, int *sum) {
     int *next = arr;
-    while(next < arr + n){
+    while (next < arr + n) {
         *sum += *next;
         next ++;
     }
