4c3867c6f8e85261724dcb46691b9b3abd1d4601
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-14 21:25:40 +0400

    Update 2_homewor.cpp

--- cpp/day7/2_homewor.cpp
+++ cpp/day7/2_homewor.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 void swap(int *num1, int *num2);
-int main()
-{
+
+int main() {
     int  num1, num2;
-    cout<<"num1:";
-    cin>>num1;
-    cout<<"num2:";
-    cin>>num2;
+    cout << "num1:";
+    cin >> num1;
+    cout << "num2:";
+    cin >> num2;
     
     int *ptr1 = &num1; 
     int *ptr2 = &num2; 
@@ -16,7 +16,7 @@ int main()
     cout << "num1:" << *ptr1 << " \n" << "num2:" << *ptr2 << endl;
     return 0;
 }
-void swap(int *num1, int *num2){
+void swap (int *num1, int *num2) {
     int elem = *num1;
     *num1 = *num2;
     *num2 = elem;
