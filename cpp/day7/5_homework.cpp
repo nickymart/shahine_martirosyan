7ad490c0cbef2cdd514b8cf82861a462e77916dd
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-14 21:21:44 +0400

    Create 5_homework.cpp

---
+++ cpp/day7/5_homework.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void findMinAndMaxElementsOfArray(int arr[], int n, int *min, int *max);
+int main()
+{
+    const int n = 10;
+    int arr[n];
+    cout << "Input:\n";
+    for(int i = 0; i < n; i ++){
+        arr[i] = rand() % 9 + 1;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int m1 = 0, m2 = 0;
+    int *min = &m1, *max = &m2;
+    findMinAndMaxElementsOfArray(arr, n, min, max);
+    cout << "min:"<< *min << " \n" <<"max:" << *max << endl;
+    return 0;
+}
+void findMinAndMaxElementsOfArray(int arr[], int n, int *min, int *max){
+    int *next = arr + n - 1;
+    *min = *next;
+    *max = *next;
+    while(next > arr - 1){
+        if(*min > *next){
+            *min = *next;
+        }
+        if(*max < *next){
+            *max = *next;    
+        }
+        next --;
+    }
+}
