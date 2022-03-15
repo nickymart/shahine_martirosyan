f98f2c43ed15e58fd07f242eb6d7e5309d2c8468
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-15 20:24:15 +0400

    Update 1_homework.cpp

--- cpp/day7/1_homework.cpp
+++ cpp/day7/1_homework.cpp
@@ -1,56 +1,45 @@
 #include <iostream>
 using namespace std;
 
-void sum1(int *a, int *b, int *sum);
-void sum2(int &a, int &b, int &sum);
-void sum3(int a, int b, int &sum);
-void sum4(int a, int *b, int &sum);
-void sum5(int *a, int *b, int &sum);
+void sum1(int *, int *, int *);
+void sum2(int &, int &, int &);
+void sum3(int , int , int &);
+void sum4(int , int *, int &);
+void sum5(int *, int *, int &);
 
 int main() {
-    int  num1, num2;
-    cout << "Enter first number:";
-    cin >> num1;
-    cout << "Enter second number:";
-    cin >> num2;
-    
-    int *summ1;
-    int *ptr1 = &num1; 
-    int *ptr2 = &num2; 
-    sum1(ptr1, ptr2, summ1);
-    cout << "sum1:" << *summ1 << endl;
-    
+    int  num1 = 10, num2 = 15;
+    cout << "num1:" << num1 << endl;
+    cout << "num2:" << num2 << endl;
     int sum;
-    int &ref1 = num1;
-    int &ref2 = num2;
-    int &summ2 = sum;
-    sum2(ref1, ref2, summ2);
-    cout << "sum2:" << summ2 << endl;
     
-    int &summ3 = sum;
-    sum3(num1, num2, summ3);
-    cout << "sum3:" << summ3  << endl;
-    
-    int &summ4 = sum;
-    sum4(num1, ptr2, summ4);
-    cout << "sum4:" << summ4 << endl;
-    
-    int &summ5 = sum;
-    sum5(ptr1, ptr2, summ4);
-    cout << "sum5:" << summ5 << endl;
+    sum1(&num1, &num2, &sum);
+    cout << "sum1:" << sum << endl;
+
+    sum2(num1, num2, sum);
+    cout << "sum2:" << sum << endl;
+
+    sum3(num1, num2, sum);
+    cout << "sum3:" << sum  << endl;
+
+    sum4(num1, &num2, sum);
+    cout << "sum4:" << sum << endl;
+
+    sum5(&num1, &num2, sum);
+    cout << "sum5:" << sum << endl;
     return 0;
 }
 void sum1(int *a, int *b, int *sum){
-    *sum = *a + *b;    
+    *sum = *a + *b;
 }
 void sum2(int &a, int &b, int &sum){
     sum = a + b;
 }
 void sum3(int a, int b, int &sum){
-    sum = a + b;    
+    sum = a + b;
 }
 void sum4(int a, int *b, int &sum){
-    sum = a + *b;    
+    sum = a + *b;
 }
 void sum5(int *a, int *b, int &sum){
     sum = *a + *b;
