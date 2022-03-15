ac3ec717f4211a80a804a39049165aebb473790f
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-15 21:36:40 +0400

    Create main.cpp

---
+++ cpp/day8/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "my_atoi/myatoi.h"
+using namespace std;
+
+int main(int argc, char** argv)
+{
+        int sum_my_atoi = 0;
+        int sum_atoi = 0;
+        for (int i = 1; i < argc; i++) {
+                sum_my_atoi += my_atoi(argv[i]);
+                sum_atoi += atoi(argv[i]);
+        }
+        cout << "sum_my_atoi:" << sum_my_atoi << endl;
+        cout << "sum_atoi:" << sum_atoi << endl;
+        return 0;
+}
