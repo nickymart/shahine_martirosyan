beca93c84f7f665f4ff2d01799ab84f965d707f7
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-23 21:20:41 +0400

    Create homework.cpp

---
+++ cpp/day12/homework.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+string correct_word(string word);
+int main() {
+	string line;
+	getline(cin, line);
+	line += " ";
+	string word;
+	int i = 0;
+	while(line[i] != '\0') {
+		word = "";
+		for(;line[i] != ' '; i++){
+			word += line[i];
+		}
+		i++;
+		cout << correct_word(word) << " ";
+	}
+	return 0;
+}
+string correct_word(string word) {
+	ifstream f1;
+	f1.open("dictionary.txt");
+	string c_word;	
+	int count = 0;
+	while(!f1.eof()) {
+		count = 0;
+		getline(f1, c_word);
+		if(c_word.size() != word.size()){
+			continue;
+		}
+		for(int i = 0; word[i] != '\0'; i++) {
+			if(word[i] != c_word[i]){
+				count ++;
+			}
+		}
+		if(count == 1) {
+			break;
+		}
+	}
+	f1.close();
+	if(count != 1) {
+		return word;
+	}
+	return c_word;
+}
