ac812f3dce34526cdac423b26c5d3b51bae76e84
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-27 17:21:48 +0400

    Update homework.cpp

--- cpp/day14/homework.cpp
+++ cpp/day14/homework.cpp
@@ -1,13 +1,83 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
-#include <locale>
-#include <ctime>
-#include <iomanip>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 using namespace std;
+
+bool getCommand();
+string fileCreate();
+void writeNote(string);
+string getFileName(string);
+string getWeekDay(int);
+int dayofweek(int d, int m, int y);
+enum week{Sunday = 0, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+struct diary {
+        string note;
+        struct data {
+            string dd; 
+            string mm; 
+            string yyyy;
+        }d;
+	week day;
+} n;
+int main() {        
+	string s = fileCreate();
+    string day = "";
+    string month = "";
+    string year = "";
+    int week;
+    string w;
+    ifstream file;
+    cout << "List your notes.\n";
+    string text;
+    for(int i = 0; s[i] != '\0'; i += 11){
+        day = s[i]; day += s[i + 1];
+        month = s[i + 3]; month += s[i + 4];
+        year = s[i + 6]; year += s[i + 7]; year += s[i + 8]; year += s[i + 9];
+        week = dayofweek(stoi(day), stoi(month), stoi(year));
+        w = getWeekDay(week);
+        cout << w << " " << day + "/" + month + "/" + year << endl;
+        file.open("./note/" + day + '_' + month + '_' + year + ".txt");
+        while(!file.eof()) {
+            getline(file, text);
+            cout << "Note:" << text << endl;
+        }
+        file.close();
+        cout << w;
+
+    }
+    return 0;
+}
+string fileCreate() {
+    bool b = true;
+    string allNotesDay = "";
+    while(b) {
+        cout << "Input dd/yy/yyyy:";
+        string data;
+        cin >> data;
+        allNotesDay += data;
+        allNotesDay += ' ';
+        string s = getFileName(data);
+        writeNote(s);
+        b = getCommand();
+    }
+    return allNotesDay;
+}
+
+void writeNote(string s){
+    ofstream file;
+    file.open(s.c_str(), ios::app);
+    cout << "Note:";
+    cin.ignore();
+    getline(cin, n.note);
+    file << n.note << endl;
+    file.close();
+}
+
 string getFileName(string data){
+    int elem = 0;
 	n.d.dd = "", n.d.mm = "", n.d.yyyy = ""; 
        	for(int i = 0; data[i] != '\0'; i++) {
 	       	if(data[i] != '/' && elem == 0) {
@@ -27,36 +97,7 @@ string getFileName(string data){
 	}	
 	return "./note/" +  n.d.dd + '_' + n.d.mm + '_' + n.d.yyyy + ".txt";
 }
-string getDayMonthYear(string data){
-	n.d.dd = "", n.d.mm = "", n.d.yyyy = ""; 
-       	for(int i = 0; data[i] != '\0'; i++) {
-	       	if(data[i] != '/' && elem == 0) {
-		       	n.d.dd += data[i];
-                        continue;
-		}else if(data[i] == '/'){
-			elem ++; 
-			i ++; 
-		}
-		if(data[i] != '/' && elem == 1) {
-			n.d.mm += data[i];
-			continue;
-		}
-		if(elem == 2){ 
-			n.d.yyyy += data[i];
-		}
-	}	
-	return n.d.dd + '/' + n.d.mm + '/' + n.d.yyyy;
-}
-enum week{
 
-                Sunday,
-                Monday,
-                Tuesday,
-                Wednesday,
-                Thursday,
-                Friday,
-                Saturday,
-};
 string getWeekDay(int day){
 	switch(day) {
 		case Sunday:
@@ -75,72 +116,22 @@ string getWeekDay(int day){
 			return "Sat";
 	}
 }
-int getWeekDay(int yy, int mm, int dd)
+
+int dayofweek(int d, int m, int y)
 {
-	int rst =
-		dd
-		+ ((153 * (mm + 12 * ((14 - mm) / 12) - 3) + 2) / 5)
-		+ (365 * (yy + 4800 - ((14 - mm) / 12)))
-		+ ((yy + 4800 - ((14 - mm) / 12)) / 4)
-		- ((yy + 4800 - ((14 - mm) / 12)) / 100)
-		+ ((yy + 4800 - ((14 - mm) / 12)) / 400)
-		- 32045;
-
-	return (rst + 1) % 7 ;
-}
-struct diary {
-        string note;
-        struct data {
-                string dd; 
-                string mm; 
-                string yyyy;
-        }d;
-	week day;
-} n;
-
-int main() {
-	ofstream filename;
-        bool b = true;
-        while(b) {
-                cout << "Input dd/yy/yyyy:";
-                string data;
-                cin >> data;
-                int elem = 0;
-                string s = getFileName(data);
-		ofstream file;
-		filename.open("filename.txt", ios::app);
-		string text;
-                getline(s, filename);
-		filename << endl;
-		file.open(s.c_str(), ios::app);
-                cout << "Note:";
-                cin.ignore();
-                getline(cin, text);
-                file << text << endl;
-                file.close();
-                string command;
-                cout << "Command:(add/exit)";
-                cin >> command;
-                if(command == "exit"){
-                        b = false;
-                }
-        }
-	filename.close();
-	cout << "List of your notes."
-	ifstream file;
-	file.open("filename.txt");
-	string s;
-	string text;
-	while(!file.eof()) {
-		getline(file, s);
-		ifstream f1;
-		f1.open(s);
-		
-		while(!f1.eof()) {
-			getline(f1, text);
-			
-		}
-	}
-        return 0;
+    static int t[] = { 0, 3, 2, 5, 0, 3,
+                       5, 1, 4, 6, 2, 4 };
+    y -= m < 3;
+    return ( y + y / 4 - y / 100 +
+             y / 400 + t[m - 1] + d) % 7;
 }
 
+bool getCommand() {
+    string command;
+    cout << "Command:(add/exit)";
+    cin >> command;
+    if(command == "exit"){
+        return false;
+    }
+    return true;
+}
