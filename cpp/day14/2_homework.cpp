493e757ce24df483bd1aa7567642446c07128aa2
nickymart <49718376+nickymart@users.noreply.github.com>
2022-03-29 23:42:26 +0400

    Update 2_homework.cpp

--- cpp/day14/2_homework.cpp
+++ cpp/day14/2_homework.cpp
@@ -4,134 +4,124 @@
 
 using namespace std;
 
-enum PhoneCodes {    Russia = 7,    Armenia = 374,    Germany = 49,    France = 33,    Georgia	= 995};
-
+enum PhoneCodes {
+        Russia = 7,    Armenia = 374,    Germany = 49,    France = 33,    Georgia  = 995
+};
+void readData();
 struct country {
-    string Country;
-    string Capital;
-    string Domain;
-    long Population;
-    long Area;
-    long GDP;
-    int Code;
-} c;
+        string Country = ""; 
+        string Capital = ""; 
+        string Domain = ""; 
+        long Population = 0;
+        long Area = 0;
+        long GDP = 0;
+        PhoneCodes Code = Russia;
+    
+} Data[5];
 void getInformation(string);
-int getPhoneCode(string);
-string searching(string);
-string searchFromCountryName(string);
-string searchFromCapital(string);
-string searchFromDomain(string);
+PhoneCodes getPhoneCode(string country);
+void searchFromCountryName(string countryName);
+void searching(string s); 
+void searchFromCountryName(string countryName);
+void readData();
 
 int main(int argc, char** argv){
-    
-    if(argv[1] == "search"){
-        string search;
+        readData();
+        /*string search;
         do {
             cout << "Input search mode (countryName / capital / domain):";
             cin >> search;
-        } while(search != "countryName" || search != "capital" || search != "domain");
-        getInformation(searching(search));
-    }
-    return 0;
+        } while(search != "countryName" && search != "capital" && search != "domain");
+        searching(search);  */  
+        return 0;
+}
+PhoneCodes getPhoneCode(string country){
+    if(country == "Russia")
+            return Russia;
+    if(country == "Armenia")
+            return Armenia;
+    if(country == "Germany") 
+            return Germany;
+    if(country == "France")
+            return France;
+    return Georgia;
+}
+void getInformation(int index) {
+    
+    cout << "Output: Country     - " << Data[index].Country << endl;
+    cout << "        Capital     - " << Data[index].Capital << endl;
+    cout << "        Domain      - " << Data[index].Domain << endl;
+    cout << "        Population  - " << Data[index].Population << endl;
+    cout << "        Area        - " << Data[index].Area << endl;
+    cout << "        GDB         - " << Data[index].GDP << endl;
+    cout << "        PhoneCode  - +" << Data[index].Code << endl;
+
 }
 
-string searching(string s){
-    string str;
-    string line;
+void  searching(string s){ 
+    string str = ""; 
     if(s == "countryName"){
         cout << "Input Country :" ;
         cin >> str;
-        line = searchFromCountryName(str);
-    }
-    else if( s == "capital"){
+        searchFromCountryName(str);
+    }   
+    /*else if( s == "capital"){
         cout << "Input Capital :" ;
         cin >> str;
-        line = searchFromCapital(str);
+        searchFromCapital(str);
     }
     else {
         cout << "Input Domain :" ;
         cin >> str;
-        line = searchFromDomain(str);
-    }
-    return line;
-}
-
-string searchFromCountryName(string countryName){
-    ifstream file;
-    file.open("countries.txt");
-    bool b = true;
-    string text = "";
-    /*while(!file.eof()){
-        b = true;
-        getline(file, text);
-        cout << text << endl;;
-        for(int i = 0; text[i] != ','; i++){
-            if(countryName[i] != text[i]){
-                b = false;
-            }
-        }
-        if(b){
-            file.close();
-            break;
-        }
+        searchFromDomain(str);
     }*/
-    getline(file, text);
-    file.close();
-    return text;
 }
 
-string searchFromCapital(string capital);
-
-string searchFromDomain(string domain);
-
-void getInformation(string line) {
-    int elem = 0;
-    string population, area,gdb;
-    for(int i = 0; line[i] != '\0'; i ++){
-        if(line[i] == ','){
-            elem ++;
-        } else {
-            if(elem = 0){
-                c.Country += line[i];
-            }
-            else if(elem = 1){
-                c.Capital += line[i];
-            }
-            else if(elem = 2){
-                c.Domain += line[i];
-            }
-            else if(elem = 3){
-                population += line[i];
-            }
-            else if(elem = 4){
-                area += line[i];
-            }
-            else {
-                gdb += line[i];
-            }
+void searchFromCountryName(string countryName){
+        for(int i = 0; i < 5; i++){
+                if(Data[i].Country == countryName){
+                        getInformation(i);
+                        break;
+                }
         }
-    }
-    c.Population = stoi(population.c_str());
-    c.GDP = stoi(gdb.c_str());
-    c.Area = stoi(area.c_str());
-    c.Code = getPhoneCode(c.Country);
-    cout << "Output: Country    - " << c.Country << endl;
-    cout << "        Capital    - " << c.Capital << endl;
-    cout << "        Domain     - " << c.Domain << endl;
-    cout << "        Population - " << c.Population << endl;
-    cout << "        Area       - " << c.Area << endl;
-    cout << "        GDB        - " << c.GDP << endl;
-    cout << "        Phone Code - +" << c.Code << endl;
 }
-
-int getPhoneCode(string country){
-    if(country == "Russia")
-            return Russia;
-    else if(country == "Armenia")
-            return Armenia;
-    else if(country == "Germany") 
-            return Germany;
-    else if(country == "France")
-            return France;
-    return Georgia;
-}
+void readData(){
+        ifstream file;
+        file.open("countries.txt");
+        int elem = 0;
+        string line;
+        string population = "", area = "",gdb = ""; 
+        while(!file.eof()) {
+            int j = 0;
+            getline(file, line);    
+            for(int i = 0; line[i] != '\0'; i++){
+                if(line[i] == ','){
+                    elem ++; 
+                    i++;
+                }else {
+                    if(elem == 0){ 
+                        Data[j].Country += line[i];
+                    } else if(elem == 1){ 
+                        Data[j].Capital += line[i];
+                    } else if(elem == 2){ 
+                        Data[j].Domain += line[i];
+                    } else if(elem == 3){ 
+                        population += line[i];
+                    } else if(elem == 4){ 
+                        area += line[i];
+                    } else {
+                        gdb += line[i];
+                    }
+                }    
+            }
+            Data[j].Population = stol(population);
+            Data[j].Area = stol(area);
+            Data[j].GDP = stol(gdb);
+            Data[j].Code = getPhoneCode(Data[j].Country);
+            cout << Data[j].Capital << endl;
+            j++;
+            population = "", area = "",gdb = "";
+            elem = 0;
+        }
+        file.close();
+}  
