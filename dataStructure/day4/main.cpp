6b127a5c5cba1e9ba31e49b0d1de00905ae1c33f
nickymart <49718376+nickymart@users.noreply.github.com>
2022-04-29 01:05:32 +0400

    Update main.cpp

--- dataStructure/day4/main.cpp
+++ dataStructure/day4/main.cpp
@@ -63,19 +63,18 @@ int main() {
     return 0;
 }
 
-void swap(int *i, int *j){
+void swap(int *i, int *j) {
     int temp = *j;
     *j = *i;
     *i = temp;
 }
 
-int quick(int *arr, int low, int high){
-
+int quick(int *arr, int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
     int temp;
     
-    for(int j = low; j < high; j++){
+    for(int j = low; j < high; j++) {
         if(arr[j] <= pivot) {
             i++;
             swap(&arr[j], &arr[i]);
@@ -85,10 +84,9 @@ int quick(int *arr, int low, int high){
     return i + 1;
 }
 
-void quickSort(int *arr, int low, int high){
-    if(low < high){
+void quickSort(int *arr, int low, int high) {
+    if(low < high) {
         int q = quick(arr, low, high);
-
         quickSort(arr, low, q - 1);
         quickSort(arr, q + 1, high);
     }
@@ -113,7 +111,7 @@ void merge(int *arr, int left, int newSize, int right) {
     int i = left;
 
     while (leftIndex < l && rightIndex < r) {
-        if(arrL[leftIndex] <= arrR[rightIndex]){
+        if(arrL[leftIndex] <= arrR[rightIndex]) {
             arr[i] = arrL[leftIndex];
             leftIndex++;
         } else {
@@ -135,8 +133,8 @@ void merge(int *arr, int left, int newSize, int right) {
     }
 }
 
-void mergeSort(int *arr, int start, int end){
-    if(start >= end){
+void mergeSort(int *arr, int start, int end) {
+    if(start >= end) {
         return;
     }
 
@@ -146,13 +144,13 @@ void mergeSort(int *arr, int start, int end){
     merge(arr, start, newSize, end);
 }
 
-void bubbleSort(int arr[]){
+void bubbleSort(int arr[]) {
     int temp;
-    for(int i = 0; i < arraySize; i++){
+    for(int i = 0; i < arraySize; i++) {
         bool swapped = false;
 
-        for(int j = 0; j < arraySize - i - 1; j++){
-            if(arr[j] > arr[j + 1]){
+        for(int j = 0; j < arraySize - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
                 swapped = true;
             }
@@ -169,33 +167,30 @@ void insertionSort(int arr[]) {
     for(int i = 1; i < arraySize; i++) {
         int j;
         int temp = arr[i];
-        for(j = i - 1; j >= 0; j --){
-            if(temp > arr[j]){
+        for(j = i - 1; j >= 0; j --) {
+            if(temp > arr[j]) {
                 break;
             }
             arr[j + 1] = arr[j];
         }
-
         arr[j + 1] = temp;
     }
 }
 
-void copyArray(int copyArray[], int newArray[], int size){
+void copyArray(int copyArray[], int newArray[], int size) {
     for(int i = 0; i < size; i++) {
         newArray[i] = copyArray[i];
     }
 }
 
 void generateRandomNumber(int arr[]) {
-
     srand(time(0));
     for(int i = 0; i < arraySize; i++) {
         arr[i] = rand() % 10000;
     }
 }
 
-void printArray(int *arr, int size)
-{
+void printArray(int *arr, int size) {
     int i;
     for (i = 0; i < size; i++)
         cout << arr[i] << " ";
