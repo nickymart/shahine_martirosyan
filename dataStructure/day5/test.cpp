3822d6fd3d00f5198c47bdedd3f0bc38b3df1799
nickymart <49718376+nickymart@users.noreply.github.com>
2022-05-03 01:28:44 +0400

    Create test.cpp

---
+++ dataStructure/day5/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "tree.hpp"
+
+using namespace std;
+
+int main(){
+
+    Tree* tree = new Tree(2);
+
+    tree->_left_tree = new Tree(3);
+    tree->_right_tree = new Tree(4);
+
+    tree->_left_tree->_left_tree = new Tree(5);
+    tree->_left_tree->_right_tree = new Tree(6);
+
+    cout << max(tree, tree->_data) << endl;
+    cout << min(tree, tree->_data) << endl;
+    cout << sum(tree) << endl;
+    insert(tree, 20);
+    dfs(tree);
+
+    return 0;
+}
