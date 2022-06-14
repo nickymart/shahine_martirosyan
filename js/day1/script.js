8520e54d1ac25c9c7b6c2c8d4ecd30b3a64c3814
nickymart <49718376+nickymart@users.noreply.github.com>
2022-06-14 22:50:40 +0400

    Add files via upload

---
+++ js/day1/script.js
@@ -0,0 +1,17 @@
+let snikersCount =  prompt("Սնիկերս");
+let colaCount =  prompt("Կոլա");
+let jermukCount =  prompt("Ջերմուկ");
+let chocolateCount =  prompt("Շոկոլադ");
+let candyCount =  prompt("Կոնֆետ");
+let paidAmount = prompt("Գումարը");
+let sum = snikersCount*200 + colaCount * 400 + jermukCount * 99.9 +chocolateCount * 1200 + candyCount * 350;
+
+console.log(`Ընդամենը: ${paidAmount}`);
+console.log(`Վերադարձ: ${paidAmount - sum}`);
+console.log(`Ծախս: ${sum}`);
+console.log('Ապրանքներ։');
+if(snikersCount > 0) console.log(` Սնիկերս ֊ ${snikersCount} հատ ֊ ${snikersCount * 200}դր`);
+if(colaCount > 0) console.log(` Կոլա ֊ ${colaCount} հատ ֊ ${colaCount * 400}դր`);
+if(jermukCount > 0) console.log(` Ջերմուկ ֊ ${jermukCount} հատ ֊ ${jermukCount * 99.9}դր`);
+if(chocolateCount > 0) console.log(` Շոկոլադ ֊ ${chocolateCount} հատ ֊ ${chocolateCount * 1200 }դր`);
+if(candyCount > 0) console.log(` Կոնֆետ ֊ ${candyCount} հատ ֊ ${candyCount * 350} դր`);
