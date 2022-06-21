af19aab4aa7c3b1d0c5c042176c444171c5c13b4
nickymart <49718376+nickymart@users.noreply.github.com>
2022-06-21 22:24:48 +0400

    Update script.js

--- js/day4/script.js
+++ js/day4/script.js
@@ -1,3 +1,6 @@
 let obj = Database.get();
 
-obj.creatTable("users" , ['username', 'password'],[['Alex','abc'], ['Shahine','ghj']]);
+obj.creatTable("users" ,['username', 'password'],[['Alex','abc'], ['Shahine','ghj']]);
+obj.creatTable("users1" ,['username', 'password'],[['Alex','abc'], ['Shahine','ghj']]);
+obj.creatTable("users2" ,['username', 'password'],[['Alex','abc'], ['Shahine','ghj']]);
+obj.creatTable("users3" ,['username', 'password'],[['Alex','abc'], ['Shahine','ghj']]);
