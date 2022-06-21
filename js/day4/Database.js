8309b443f7551b090b471b4a680379013fc78d25
nickymart <49718376+nickymart@users.noreply.github.com>
2022-06-21 22:25:12 +0400

    Update Database.js

--- js/day4/Database.js
+++ js/day4/Database.js
@@ -7,21 +7,45 @@ class Database {
     }
     creatTable(name, colume, values = null) {
         let table = new Table(name, colume, values);
-        this.save(table);
+        let arr = JSON.parse(localStorage.getItem('db'));
+        arr.push(table);
+        this.save(arr);
     }
 
     getTable(name) {
         let arr = JSON.parse(localStorage.getItem('db'));
         for (let key in arr) {
             if (name == arr[key].name) {
-                return arr[key];
+                return arr[key].name, arr[key].columns, arr[key].values;
             }
         }
     }
 
-    save(table) {
-        let arr = JSON.parse(localStorage.getItem('db'));
-        arr.push(table);
+    save(arr) {
         localStorage.setItem('db', JSON.stringify(arr));
+    }
+
+    delete(id) {
+        let arr = JSON.parse(localStorage.getItem('db'));
+        arr.splice(id - 1, 1)
+        this.save(arr);
+    }
+
+    get(id){
+        let arr = JSON.parse(localStorage.getItem('db'));
+        return arr[id - 1];
+    }
+
+    update(name, newname, columns, values) {
+        let arr = JSON.parse(localStorage.getItem('db'));
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i].name == name) {
+                arr[i].name = newname;
+                arr[i].columns = columns;
+                arr[i].values = values;
+                this.save(arr);
+                return;
+            }
+        }
     }
 }
